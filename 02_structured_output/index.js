13d824fedc71d15dcdf6f38085da6fb776b07cbc
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-01-06 23:42:43 +0530

    init some packages

---
+++ 02_structured_output/index.js
@@ -0,0 +1,34 @@
+import { Agent, run, tool } from "@openai/agents";
+import dotenv from "dotenv";
+import { z } from "zod";
+import axios from "axios";
+
+dotenv.config();
+
+const getWeatherTool = tool({
+  name: "get_weather",
+  description: "returns the current weather information for the given city",
+  parameters: z.object({
+    city: z.string().describe("name of the city"),
+  }),
+  execute: async function ({ city }) {
+    const url = `https://wttr.in/${city.toLowerCase()}?format=%C+%t`;
+    const response = await axios.get(url, { responseType: "text" });
+    return `The weather of ${city} is ${response.data}`;
+  },
+});
+
+const agent = new Agent({
+  name: "Weather Agent",
+  instructions: `
+        You are an expert weather agent that helps user to tell weather report and give only temperature.
+    `,
+  tools: [getWeatherTool],
+});
+
+async function main(query = "") {
+  const result = await run(agent, query);
+  console.log(`Result:`, result.finalOutput);
+}
+
+main(`What is the weather of Goa, Delhi and Patiala?`);
