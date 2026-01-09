9748c34f5babc36b4566471efbc26f78f2eda013
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-01-09 22:29:19 +0530

    add tool and agent

--- 03_multi_agent/index.js
+++ 03_multi_agent/index.js
@@ -1,6 +1,17 @@
 import { Agent, tool } from "@openai/agents";
+import { z } from "zod";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const salesAgent = new Agent({});
+const fetchAvailablePlans = tool({
+  name: "fetch_available",
+  description: "fetches the available plans for the internet",
+  parameters: undefined,
+});
+
+const salesAgent = new Agent({
+  name: "Sales Agent ",
+  instructions: `You are an expert sales agent for an internet broadband company.
+    Talk to the user and help them with what they need.`,
+});
